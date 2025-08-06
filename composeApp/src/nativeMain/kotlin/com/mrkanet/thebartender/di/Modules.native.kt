fc68507840cd0b0e3ff4a4228ef33ca90b3ba013
vektorkaan <mrkaan888@yandex.com>
2025-08-06 13:25:18 +0300

    firebase integration by ai

--- composeApp/src/nativeMain/kotlin/com/mrkanet/thebartender/di/Modules.native.kt
+++ composeApp/src/nativeMain/kotlin/com/mrkanet/thebartender/di/Modules.native.kt
@@ -1,9 +1,6 @@
 package com.mrkanet.thebartender.di
 
-import com.mrkanet.thebartender.dependencies.DBClient
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformModule = module {
-    singleOf(::DBClient)
 }
